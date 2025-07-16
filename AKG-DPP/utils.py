f47c506cb71087e43f637fb6bd3f74f6970dfeda
Khoadoan30028060 <146908560+Khoadoan30028060@users.noreply.github.com>
2025-07-16 10:35:59 +0700

    .

--- AKG-DPP/utils.py
+++ AKG-DPP/utils.py
@@ -16,7 +16,6 @@ from pycocoevalcap.rouge.rouge import Rouge
 from pycocoevalcap.cider.cider import Cider
 from pycocoevalcap.meteor.meteor import Meteor
 
-from config import get_graph
 import gc
 
 
